87b79ff1fc6b2a1e522889bf809cf7778afe23a0
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-06-25 01:38:13 -0700

    fix kinde ssr issues

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -3,26 +3,38 @@ import {
 	PUBLIC_KINDE_DOMAIN,
 	PUBLIC_KINDE_REDIRECT_URI
 } from '$env/static/public';
-import createKindeClient from '@kinde-oss/kinde-auth-pkce-js';
+import createKindeClient, { type KindeClient } from '@kinde-oss/kinde-auth-pkce-js';
 
-const kindePromise = createKindeClient({
-	audience: 'trading-server-api',
-	client_id: PUBLIC_KINDE_CLIENT_ID,
-	domain: PUBLIC_KINDE_DOMAIN,
-	redirect_uri: PUBLIC_KINDE_REDIRECT_URI
-});
+import { browser } from '$app/environment';
 
-export const kinde = {
-	async login() {
-		const kinde = await kindePromise;
-		kinde.login();
-	},
-	async logout() {
-		const kinde = await kindePromise;
-		kinde.logout();
-	},
-	async isAuthenticated() {
-		const kinde = await kindePromise;
-		return kinde.isAuthenticated();
-	}
-};
+let kindePromise: Promise<KindeClient>;
+
+if (browser) {
+	kindePromise = createKindeClient({
+		audience: 'trading-server-api',
+		client_id: PUBLIC_KINDE_CLIENT_ID,
+		domain: PUBLIC_KINDE_DOMAIN,
+		redirect_uri: PUBLIC_KINDE_REDIRECT_URI
+	});
+}
+
+export const kinde = browser
+	? {
+			async login() {
+				const kinde = await kindePromise;
+				kinde.login();
+			},
+			async logout() {
+				const kinde = await kindePromise;
+				kinde.logout();
+			},
+			async isAuthenticated() {
+				const kinde = await kindePromise;
+				return kinde.isAuthenticated();
+			}
+		}
+	: {
+			login: () => {},
+			logout: () => {},
+			isAuthenticated: () => Promise.resolve(false)
+		};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
 		proxy: {
 			'/api': 'http://localhost:8080'
 		}
+	},
+	ssr: {
+		noExternal: ['@kinde-oss/kinde-auth-pkce-js']
 	}
 });
