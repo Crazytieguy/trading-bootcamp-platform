ddee7b659c579dbda70d24bbcc5bede27429d00e
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-07-11 03:15:55 -0700

    improve checkpointing

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,4 +1,4 @@
-use std::{env, str::FromStr, time::Duration};
+use std::{env, str::FromStr};
 
 use sqlx::{
     sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions, SqliteSynchronous},
@@ -6,9 +6,8 @@ use sqlx::{
 };
 use tokio::sync::broadcast::error::RecvError;
 
-// should hopefully keep the WAL size nice and small
-// and avoid blocking writers
-const CHECKPOINT_AFTER_RELEASES: u64 = 128;
+// should hopefully keep the WAL size nice and small and avoid blocking writers
+const CHECKPOINT_PAGE_LIMIT: i64 = 512;
 
 pub async fn init() -> anyhow::Result<SqlitePool> {
     let connection_options = SqliteConnectOptions::from_str(&env::var("DATABASE_URL")?)?
@@ -17,7 +16,9 @@ pub async fn init() -> anyhow::Result<SqlitePool> {
         .synchronous(SqliteSynchronous::Normal)
         // This should work with the default idle timeout and max lifetime
         .optimize_on_close(true, None)
-        .pragma("optimize", "0x10002");
+        .pragma("optimize", "0x10002")
+        // already checkpointing in the background
+        .pragma("wal_autocheckpoint", "0");
 
     let mut management_conn = SqliteConnection::connect_with(&connection_options).await?;
 
@@ -42,32 +43,51 @@ pub async fn init() -> anyhow::Result<SqlitePool> {
 
     // checkpointing task
     tokio::spawn(async move {
-        let mut released = 0;
+        let mut released_connections = 0;
+        let mut remaining_pages = 0;
         loop {
             match release_rx.recv().await {
                 Ok(()) => {
-                    released += 1;
+                    released_connections += 1;
                 }
                 Err(RecvError::Lagged(n)) => {
-                    released += n;
+                    released_connections += n as i64;
                 }
                 Err(RecvError::Closed) => {
                     break;
                 }
             };
-            if released >= CHECKPOINT_AFTER_RELEASES {
-                released = 0;
-                if let Err(e) = sqlx::query("PRAGMA wal_checkpoint(PASSIVE)")
-                    .execute(&mut management_conn)
-                    .await
-                {
-                    tracing::error!("wal_checkpoint failed: {:?}", e);
-                };
+            let approx_wal_pages = remaining_pages + released_connections * 4;
+            if approx_wal_pages < CHECKPOINT_PAGE_LIMIT {
+                continue;
             }
-            // Don't bother recieving every release
-            tokio::time::sleep(Duration::from_millis(100)).await;
+            match sqlx::query_as::<_, WalCheckPointRow>("PRAGMA wal_checkpoint(PASSIVE)")
+                .fetch_one(&mut management_conn)
+                .await
+            {
+                Err(e) => {
+                    tracing::error!("wal_checkpoint failed: {:?}", e);
+                }
+                Ok(row) => {
+                    released_connections = 0;
+                    remaining_pages = row.log - row.checkpointed;
+                    tracing::info!(
+                        "wal_checkpoint: busy={} log={} checkpointed={}",
+                        row.busy,
+                        row.log,
+                        row.checkpointed
+                    );
+                }
+            }
         }
     });
 
     Ok(pool)
+}
+
+#[derive(sqlx::FromRow)]
+struct WalCheckPointRow {
+    busy: i64,
+    log: i64,
+    checkpointed: i64,
 }
