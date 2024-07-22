152a8b1625a7620a076b4e15fea9efe80351fa12
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-07-21 19:21:29 -0700

    try adding a fallback

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ fallback: '200.html' })
 	}
 };
 
