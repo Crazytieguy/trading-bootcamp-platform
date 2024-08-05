f9bb7de658ea845716400df8d222b19d012298f4
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-08-05 04:03:12 -0700

    add payments table

--- frontend/src/routes/payments/+page.svelte
+++ frontend/src/routes/payments/+page.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
 	import { actingAs, payments, users } from '$lib/api';
 	import MakePayment from '$lib/components/forms/makePayment.svelte';
+	import * as Table from '$lib/components/ui/table/index.js';
 </script>
 
 <h2 class="mb-4 text-xl font-bold">Payments</h2>
 <MakePayment />
-{#each $payments as { amount, payerId, recipientId, note, id } (id)}
-	<div class="my-4 flex items-center gap-4">
-		<p>
-			payer: {payerId === $actingAs ? 'You' : $users.get(payerId ?? '')?.name || 'Unnamed user'}
-		</p>
-		<p>
-			recipient: {recipientId === $actingAs
-				? 'You'
-				: $users.get(recipientId ?? '')?.name || 'Unnamed user'}
-		</p>
-		<p>amount: {amount}</p>
-		<p>note: {note}</p>
-	</div>
-{/each}
+<Table.Root class="text-center">
+	<Table.Header>
+		<Table.Row>
+			<Table.Head class="text-center">Payer</Table.Head>
+			<Table.Head class="text-center">Recipient</Table.Head>
+			<Table.Head class="text-center">Amount</Table.Head>
+			<Table.Head class="text-center">Note</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each $payments as { amount, payerId, recipientId, note, id } (id)}
+			<Table.Row>
+				<Table.Cell>
+					{payerId === $actingAs ? 'You' : $users.get(payerId ?? '')?.name || 'Unnamed user'}
+				</Table.Cell>
+				<Table.Cell>
+					{recipientId === $actingAs
+						? 'You'
+						: $users.get(recipientId ?? '')?.name || 'Unnamed user'}
+				</Table.Cell>
+				<Table.Cell>{amount}</Table.Cell>
+				<Table.Cell>{note}</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
