07957a151b45cc197776294cc9f2ec09dc07a95c
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-08-11 02:26:55 -0700

    add act_as to bot script

--- client-py/main.py
+++ client-py/main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from naive_bot import naive_bot
 from state import State
 from typing_extensions import Annotated
-from websocket_api import Authenticate, ClientMessage
+from websocket_api import ActAs, Authenticate, ClientMessage
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
@@ -18,19 +18,22 @@ def main(
     api_url: Annotated[str, typer.Argument(envvar="API_URL")],
     jwt: Annotated[str, typer.Argument(envvar="JWT")],
     id_jwt: Annotated[str, typer.Argument(envvar="ID_JWT")],
+    act_as: Annotated[str, typer.Argument(envvar="ACT_AS")],
 ):
-    asyncio.run(entrypoint(api_url, jwt, id_jwt))
+    asyncio.run(entrypoint(api_url, jwt, id_jwt, act_as))
 
 
-async def entrypoint(api_url: str, jwt: str, id_jwt: str):
+async def entrypoint(api_url: str, jwt: str, id_jwt: str, act_as: str):
     async with websockets.connect(api_url) as ws:
         authenticate = ClientMessage(authenticate=Authenticate(jwt=jwt, id_jwt=id_jwt))
         await ws.send(bytes(authenticate))
+        act_as_msg = ClientMessage(act_as=ActAs(user_id=act_as))
+        await ws.send(bytes(act_as_msg))
 
         state = State()
         await state.init(ws)
         await naive_bot(
-            state, ws, market_id=1, loss_per_trade=Decimal(1), seconds_per_trade=5.0
+            state, ws, market_id=2, loss_per_trade=Decimal("0.1"), seconds_per_trade=2.0
         )
 
 
