b0b050fe497386c1e92644b87939823dc234dd71
Nicholas Charette <charettnr@gmail.com>
2024-11-10 12:00:03 -0800

    Fix redemptions (from old 'redeem a settled market' PR)

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -175,19 +175,15 @@ impl DB {
         }
 
         let settled = sqlx::query!(
-            r#"WITH fund_and_constituents AS (
-                SELECT ? AS fund_id
-                UNION ALL
-                SELECT constituent_id
-                FROM redeemable
-                WHERE fund_id = ?
-            ) SELECT id FROM market WHERE id IN (SELECT id FROM fund_and_constituents) AND settled_price IS NOT NULL"#,
-            fund_id,
+            r#"SELECT EXISTS (
+                SELECT 1 FROM market JOIN redeemable ON (market.id = fund_id or market.id = constituent_id) WHERE fund_id = ? AND settled_price IS NOT NULL
+            ) as "exists!: bool""#,
             fund_id
-        ).fetch_all(transaction.as_mut())
-        .await?;
+        ).fetch_one(transaction.as_mut())
+        .await?
+        .exists;
 
-        if !settled.is_empty() {
+        if settled {
             return Ok(RedeemStatus::MarketSettled);
         }
 
