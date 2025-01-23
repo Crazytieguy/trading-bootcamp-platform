a214de4d6ce8c1f697548e6fff496539db694294
Yoav Tzfati <yoav.tzfati@gmail.com>
2025-01-23 02:33:14 -0500

    give new users 2000 clips

--- backend/src/handle_socket.rs
+++ backend/src/handle_socket.rs
@@ -983,7 +983,11 @@ async fn authenticate(
                         }
                     };
                 let is_admin = valid_client.roles.contains(&Role::Admin);
-                let initial_balance = if is_admin { dec!(1_000_000) } else { dec!(0) };
+                let initial_balance = if is_admin {
+                    dec!(1_000_000)
+                } else {
+                    dec!(2000)
+                };
                 let status = app_state
                     .db
                     .ensure_user_created(
