0f473b219c386ee1236cf59cb62dba68d56d1535
Yoav Tzfati <yoav.tzfati@gmail.com>
2025-01-23 23:52:11 -0500

    display coowners

--- frontend/src/routes/accounts/+page.svelte
+++ frontend/src/routes/accounts/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { PUBLIC_SERVER_URL } from '$env/static/public';
-	import { serverState } from '$lib/api.svelte';
+	import { accountName, serverState } from '$lib/api.svelte';
 	import { kinde } from '$lib/auth.svelte';
 	import CreateAccount from '$lib/components/forms/createAccount.svelte';
 	import ShareOwnership from '$lib/components/forms/shareOwnership.svelte';
@@ -20,12 +20,26 @@ ACT_AS=${serverState.actingAs}
 		navigator.clipboard.writeText(envString);
 		toast.success('Environment variables copied to clipboard');
 	};
+
+	let coOwners = $derived(
+		serverState.portfolio?.ownerCredits
+			?.filter((ownerCredit) => ownerCredit.ownerId !== serverState.userId)
+			.map((ownerCredit) => ownerCredit.ownerId) || []
+	);
 </script>
 
 <div class="mr-auto flex flex-col gap-8 pt-8">
 	<div>
 		<h1 class="text-xl font-bold">Accounts</h1>
 		{#if serverState.actingAs && serverState.accounts.get(serverState.actingAs)}
+			<p>
+				Currently acting as {accountName(serverState.actingAs)}
+			</p>
+			{#if coOwners.length > 0}
+				<p>
+					Co-owned by {coOwners.map((owner) => accountName(owner)).join(', ')}
+				</p>
+			{/if}
 			<h3 class="mt-4">
 				<Button variant="outline" onclick={copyEnv}>
 					<Copy class="mr-2 size-4" /> Copy environment variables
