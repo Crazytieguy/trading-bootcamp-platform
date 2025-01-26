0bb917f72ab5da8b682525e7df15480fc6ae1306
Dmytro S Lituiev <d.lituiev@gmail.com>
2025-01-26 15:07:05 -0500

    wrapped in a loop

--- python-client/etf_arbitrage.py
+++ python-client/etf_arbitrage.py
@@ -1,6 +1,43 @@
 from metagame.websocket_api import CancelOrder, ClientMessage, CreateOrder, Side
 from price_utils import MarketPrices, bcolors
 from pydantic import BaseModel
+import logging
+from time import sleep
+from typing import Annotated, Optional
+
+import typer
+from dotenv import load_dotenv
+from metagame import TradingClient
+from metagame.websocket_api import CancelOrder, ClientMessage, CreateOrder, Side
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+load_dotenv()
+
+app = typer.Typer(pretty_exceptions_show_locals=False)
+
+@app.command()
+def main(
+    jwt: Annotated[str, typer.Option(envvar="JWT")],
+    api_url: Annotated[str, typer.Option(envvar="API_URL")],
+    act_as: Annotated[int, typer.Option(envvar="ACT_AS")],
+    market_name: str,
+    spread: float = 1.0,
+    size: float = 1.0,
+    fade_per_order: float = 1.0,
+    prior: Optional[float] = None,
+):
+    with TradingClient(api_url, jwt, act_as) as client:
+        arbitrage_etf_bot(
+            client,
+            market_name=market_name,
+            spread=spread,
+            size=size,
+            fade_per_order=fade_per_order,
+            prior=prior,
+            dry=True
+        )
+
 
 class PComponent(BaseModel):
     weight: float
@@ -15,7 +52,7 @@ class PComponent(BaseModel):
             name=name
         )
 
-def arbitrage_etf(portfolio:list[MarketPrices], etf_name:str, max_size:float=1.0):
+def arbitrage_etf(portfolio:list[MarketPrices], etf_name:str, max_size:float=1.0, dry:bool=False):
 
     etf_prices = MarketPrices.from_client(client, etf_name)
 
@@ -29,70 +66,124 @@ def arbitrage_etf(portfolio:list[MarketPrices], etf_name:str, max_size:float=1.0
     if etf_prices.bid > denovo_etf_sum.offer:
         size = min([max_size, etf_prices.bid_size, denovo_etf_sum.offer_size])   # check for the size of the trad
         # buy raw components and sell ETF
-        side = Side.BUY
-
+        
+        bids = []
         for market, prices in zip(portfolio, denovo_etf_components):
             market_id = market.id
             price = prices.bid / market.weight
             size = size / market.weight
             print(bcolors.OKGREEN + f"buy {market.name} at {price}" + bcolors.ENDC)
-            client.create_order(
+            
+            bid_ = ClientMessage(
+                create_order=CreateOrder(
                     market_id=market_id,
-                    side=side,
-                    size=size,
                     price=price,
+                    size=size,
+                    side=Side.BID,
+                )
             )
+            bids.append(bid_)
         # sell ETF
         print(bcolors.RED + f"sell {etf_prices.name} at {etf_prices.bid}" + bcolors.ENDC)
-        client.create_order(
+        offers = [ClientMessage(
+                create_order=CreateOrder(
                     market_id=etf_prices.id,
-                    side=Side.OFFER,
-                    size=size,
                     price=etf_prices.bid,
-            )
+                    size=size,
+                    side=Side.OFFER,
+                )
+            )]
+        if not dry:
+            client.request_many(bids + offers)
+
+        for market in ([etf_prices] + portfolio):
+            client.out(market.id)
     elif etf_prices.offer < denovo_etf_sum.bid:
-        # 
-        size = min([max_size, denovo_etf_sum.bid_size, etf_prices.offer_size])
         # buy ETF and sell raw components
-        
+        size = min([max_size, denovo_etf_sum.bid_size, etf_prices.offer_size])
         # buy ETF
         print(f"buy {etf_prices.name} at {etf_prices.offer}")
-        client.create_order(
-            market_id=etf_prices.id,
-            side=side.BID,
-            size=size,
-            price=etf_prices.offer,
-        )
+        bids = [ClientMessage(
+                create_order=CreateOrder(
+                market_id=etf_prices.id,
+                side=side.BID,
+                size=size,
+                price=etf_prices.offer,
+        ))]
         # sell components
+        offers = []
         for market, prices in zip(portfolio, denovo_etf_components):
             market_id = market.id
             price = prices.offer / market.weight
             size = size / market.weight
             print(f"sell {market.name} at {price}")
-            client.create_order(
+            offer_ = ClientMessage(
+                create_order=CreateOrder(
                 market_id=market_id,
                 side=Side.OFFER,
                 size=size,
                 price=price,
-            )
+            ))
+            offers.append(offer_)
+        if not dry:
+            client.request_many(bids + offers)
+        # client.out(market_id)
+        for market in (portfolio + [etf_prices]):
+            client.out(market.id)
     else:
         print("No arbitrage opportunity")
 
-if __name__ == '__main__':
-    from dotenv import load_dotenv, dotenv_values
-    from metagame import TradingClient
-    import os
 
-    env = dotenv_values()
+def arbitrage_etf_bot(
+    client: TradingClient,
+    *,
+    market_name: str,
+    spread: float,
+    size: float,
+    fade_per_order: float,
+    prior: Optional[float] = None,
+    dry: bool = False,
+) -> None:
+    # Clear out any existing orders
+    market_id = client.state().market_name_to_id[market_name]
+    client.out(market_id)
+    logger.info(f"Starting market maker bot for market {market_name}")
 
-    client = TradingClient(
-        api_url=env["API_URL"],
-        jwt=env["JWT"],
-        act_as=int(env["ACT_AS"]),
-    )
-    portfolio = [PComponent.from_name(client, name="test_mix_max", weight=2), 
-                PComponent.from_name(client, name="Will any team go bankrupt via a rogue bot?", weight=1), 
-                PComponent.from_name(client, name="Will any team go bankrupt via a rogue bot?", weight=1)]
-    etf_name = "Will any team go bankrupt via a rogue bot?"
+    etf_name = "def_tradewars"
 
-    arbitrage_etf(portfolio, etf_name)
+    portfolio = [PComponent.from_name(client, name="delta_tradewars", weight=1), 
+                PComponent.from_name(client, name="echo_tradewars", weight=1), 
+                PComponent.from_name(client, name="foxtrot_tradewars", weight=4)]
+
+    while True:
+        sleep(1)
+        state = client.state()
+        market = state.markets.get(market_id)
+        if market is None:
+            logger.info(f"No market data available for market {market_id}")
+            continue
+
+        arbitrage_etf(portfolio, etf_name, dry=dry)
+
+if __name__ == "__main__":
+    app()
+
+
+# if __name__ == '__main__':
+#     from dotenv import load_dotenv, dotenv_values
+#     from metagame import TradingClient
+#     import os
+
+#     env = dotenv_values()
+
+#     client = TradingClient(
+#         api_url=env["API_URL"],
+#         jwt=env["JWT"],
+#         act_as=int(env["ACT_AS"]),
+#     )
+#     portfolio = [PComponent.from_name(client, name="test_mix_max", weight=2), 
+#                 PComponent.from_name(client, name="Will any team go bankrupt via a rogue bot?", weight=1), 
+#                 PComponent.from_name(client, name="Will any team go bankrupt via a rogue bot?", weight=1)]
+#     etf_name = "Will any team go bankrupt via a rogue bot?"
+
+#     arbitrage_etf(portfolio, etf_name)
