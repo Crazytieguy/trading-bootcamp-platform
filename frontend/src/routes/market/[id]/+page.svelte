48cfa5cf3c01bcffef369238ff5f19444828a0a5
paulyea <paul@getazimuth.com>
2025-02-15 22:19:58 +0000

    Check in

--- frontend/src/routes/market/[id]/+page.svelte
+++ frontend/src/routes/market/[id]/+page.svelte
@@ -4,15 +4,28 @@
 	import Market from '$lib/components/market.svelte';
 
 	let id = $derived(Number($page.params.id));
-	let marketData = $derived(Number.isNaN(id) ? undefined : serverState.markets.get(id));
+	// let marketData0 = $derived(Number.isNaN(id) ? undefined : serverState.markets.get(id));
+	let marketData0 = $derived(serverState.markets.get(52)); // Hardcoded ID 1
+	let marketData1 = $derived(serverState.markets.get(53)); // Hardcoded ID 2
+	let marketData2 = $derived(serverState.markets.get(54)); // Hardcoded ID 3
 </script>
 
 <div class="flex-grow py-8">
 	{#if serverState.actingAs}
-		{#if marketData}
-			<Market {marketData} />
-		{:else}
-			<p>Market not found</p>
-		{/if}
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+			{#if marketData0}
+				<Market marketData={marketData0} />
+			{/if}
+		</div>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+			{#if marketData1}
+				<Market marketData={marketData1} />
+			{/if}
+		</div>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+			{#if marketData2}
+				<Market marketData={marketData2} />
+			{/if}
+		</div>
 	{/if}
 </div>
